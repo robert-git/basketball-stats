05a0aaeee3a371177c9a21f358592fd2b93d72bd
robert-git <robertpulumbarit@gmail.com>
2019-02-21 00:58:39 -0600

    Add reference to implementation file

--- src/trueShootingPct.js
+++ src/trueShootingPct.js
@@ -2,6 +2,8 @@
 
 var BasketballStats = ( function() {
   var _ts = function( pts, fga, fta ) {
+    // TS% definition: https://www.basketball-reference.com/about/glossary.html
+
     var tsa = fga + 0.44 * fta;
     return pts / ( 2 * tsa );
   };
